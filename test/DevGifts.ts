e6fff4b788f983583e4b6accf7c37f53091fcd4d
victor <873602646@qq.com>
2023-12-21 14:10:29 +0800

    feat: update test console

--- test/DevGifts.ts
+++ test/DevGifts.ts
@@ -41,15 +41,29 @@ describe("DevGifts contract", function () {
 			deployTokenFixture
 		)
 
+		const ownerBalance_start = await hardhatToken.balanceOf(owner.address)
+		console.log("ownerBalance-start", ownerBalance_start)
+
 		// Transfer 50 tokens from owner to addr1
 		await expect(
 			hardhatToken.transfer(addr1.address, 50)
 		).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50])
 
+		const address1Balance = await hardhatToken.balanceOf(addr1.address)
+		console.log("address1Balance", address1Balance)
+
 		// Transfer 50 tokens from addr1 to addr2
 		// We use .connect(signer) to send a transaction from another account
-		// await expect(
-		// 	hardhatToken.connect(addr1).transfer(addr2.address, 50)
-		// ).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50])
+		await expect(
+			hardhatToken.connect(addr1).transfer(addr2.address, 50)
+		).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50])
+
+		// after all transactions done, ownerBalance should be 2950, address1 is 0 and address2 is 50
+		const ownerBalance = await hardhatToken.balanceOf(owner.address)
+		console.log("ownerBalance-later", ownerBalance)
+		const address1Balance_later = await hardhatToken.balanceOf(addr1.address)
+		console.log("address1Balance_later", address1Balance_later)
+		const address2Balance_later = await hardhatToken.balanceOf(addr2.address)
+		console.log("address2Balance_later", address2Balance_later)
 	})
 })
